1fb8cfcb38b082901c5ab1680dcf549d8de18e8f
EliMilos <elian.mancero@gmail.com>
2025-03-26 18:16:52 -0500

    Prueba

--- Prueba/Program.cs
+++ Prueba/Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Probando GIT");
+
+
+int num1, num2;
+num1 = 0;
+num2 = 2;
+
+int suma;
+
+if (num1 > num2)
+{
+    suma = num1 + num2;
+}
+
+Console.WriteLine("Suma " + suma);
